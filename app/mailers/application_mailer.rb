a533d7d5762383a5c859ba621efc66d5dbef7ae3
John Wilger <john@johnwilger.com>
2021-02-05 07:41:02 -0800

    Use ENV var for FROM addresses in mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '"Team Oregon Website" <no-reply@mg.teamoregon.org>'
+  default from: "\"Team Oregon Website\" <#{ENV.fetch('TEAMO_CONTACT_FORM_FROM', 'no-reply@test.example')}>"
   layout 'mailer'
 end
